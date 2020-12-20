b67b2baabd9129641a69b1db8aa9f56dbfa2d5f4
aliostadi <ali.ostadiy@gmail.com>
2020-12-20 10:45:26 -0800

    add .py file and delete test file

---
+++ func.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import pandas as pd
+
+
+# In[2]:
+
+
+def dataformat(data):
+    data=data.iloc[:,:-2]
+    return data
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
